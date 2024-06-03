f22f3849bac02fe355608d6cb8bbc188258cb3de
Omar <omarescamez@gmail.com>
2024-06-03 18:28:56 +0200

    FooterImprovedFavIconsAdded

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,14 @@
 import { NavLink,useLocation} from "react-router-dom";
 
-
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
 export function Footer(){
 
@@ -12,28 +20,70 @@ export function Footer(){
 
 
     return(
-        <footer className=" p-3" style={{backgroundColor:"rgba(0,71,171,1",display: isRinconesPage ? "none" : "block" }}>
-            <div className="container text-center">
+        // <footer className=" p-3" style={{backgroundColor:"rgba(0,71,171,1",display: isRinconesPage ? "none" : "block" }}>
+        //     <div className="container text-center">
 
 
-                <nav className="d-flex justify-content-evenly mb-2">
+        //         <nav className="d-flex justify-content-evenly mb-2">
 
 
-                    <a href="http://facebook.com" target="_blank" className="enlacecontenedor"><i className="bi bi-facebook fs-3" ></i></a>
-                    <a href="http://twitter.com" target="_blank"><i className="bi bi-twitter fs-3"></i></a>
-                    <a href="http://github.com" target="_blank"><i className="bi bi-github fs-3"></i></a>
-                    <a href="http://youtube.com" target="_blank"><i className="bi bi-youtube fs-3"></i></a>
-                    <a href="http://instagram.com" target="_blank"><i className="bi bi-instagram fs-3"></i></a>
-                    <a href="http://m.me/user.com" target="_blank"><i className="bi bi-messenger fs-3"></i></a>
-                    <a href="http://whatsapp.com/send?phone=number" target="_blank"><i
-                            className="bi bi-whatsapp fs-3"></i></a>
+        //             <a href="http://facebook.com" target="_blank" className="enlacecontenedor"><i className="bi bi-facebook fs-3" ></i></a>
+        //             <a href="http://twitter.com" target="_blank"><i className="bi bi-twitter fs-3"></i></a>
+        //             <a href="http://github.com" target="_blank"><i className="bi bi-github fs-3"></i></a>
+        //             <a href="http://youtube.com" target="_blank"><i className="bi bi-youtube fs-3"></i></a>
+        //             <a href="http://instagram.com" target="_blank"><i className="bi bi-instagram fs-3"></i></a>
+        //             <a href="http://m.me/user.com" target="_blank"><i className="bi bi-messenger fs-3"></i></a>
+        //             <a href="http://whatsapp.com/send?phone=number" target="_blank"><i
+        //                     className="bi bi-whatsapp fs-3"></i></a>
 
 
-                </nav>
-                <small className="text-white ">&copy; 2024 Autocine Omar Escámez</small>
-            </div>
+        //         </nav>
+        //         <small className="text-white ">&copy; 2024 Autocine Omar Escámez</small>
+        //     </div>
 
-        </footer>
+        // </footer>
+        
+      <footer className="footer" style={{ backgroundColor:"rgba(0,71,171,1)",       display: isRinconesPage ? "none" : "block"    }}>
+
+
+        <div className="footer-social-media">
+            <ul className="social-media">
+                <li className="social-media-item">
+                    <a href="#" className="social-media-link">
+                        <i className="fab fa-facebook-square"></i>
+                    </a>
+                </li>
+
+                <li className="social-media-item">
+                    <a href="#" className="social-media-link">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                </li>
+
+                <li className="social-media-item">
+                    <a href="#" className="social-media-link">
+                        <i className="fab fa-google-plus-square"></i>
+                    </a>
+                </li>
+
+                <li className="social-media-item">
+                    <a href="#" className="social-media-link">
+                        <i className="fab fa-youtube"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div className="footer-copyright">
+            <p className="footer-copyright-paragraph">
+                &copy; Copyeight. 2024.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; NONINÁ - Agenda Gastro-Socio-Cultural
+            </p>
+        </div>
+
+
+
+    </footer>
+
     );
 }
 
